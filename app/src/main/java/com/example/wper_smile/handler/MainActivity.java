39b3b1323cc6d319d41bd64e16d76323d44922f9
wper-smile <775016131@qq.com>
2016-09-27 23:03:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wper_smile/handler/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.wper_smile.handler;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView showView=(TextView)findViewById(R.id.showView);
+
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                showView.setText(msg.arg1+"");
+            }
+        };
+
+        final Runnable myWorker = new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while(progress <= 100){
+                    Message msg = new Message();
+                    msg.arg1 = progress;
+                    handler.sendMessage(msg);
+                    progress += 10;
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage();//同 new Message();
+                msg.arg1 = -1;
+                handler.sendMessage(msg);
+            }
+        };
+
+
+        Button button = (Button) findViewById(R.id.staBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null, myWorker, "WorkThread");
+                workThread.start();
+            }
+        });
+
+    }
+}
